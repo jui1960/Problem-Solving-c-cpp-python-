6e1c91cdb32aa9cbb8a7ddd092aa3ebe0d46e45f
jui1960 <juifull196044@gmail.com>
2025-11-28 21:05:08 +0600

    sum

---
+++ 2.loop/S_Sum_of_Consecutive_Odd_Numbers.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+int main()
+{
+    int T;
+    scanf("%d", &T);
+
+    for (int j = 1; j <= T; j++)
+    {
+        int X, Y;
+        scanf("%d %d", &X,&Y);
+        int sum = 0;
+        if (X > Y)
+        {
+            int temp = X;
+            X = Y;
+            Y = temp;
+        }
+       
+        for (int i = X+1; i<Y; i++)
+
+        {
+
+            if (i % 2 != 0)
+            {
+
+                sum = sum + i;
+            }
+        }
+        printf("%d\n", sum);
+    }
+
+    return 0;
+}
